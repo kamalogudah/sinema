a6b9845e7d6c0448582ba7ee00ae1b190b6ce62f
kamalogudah <mcpaul2058@gmail.com>
2018-07-26 12:29:04 +0300

    Nesting Templates

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -5,4 +5,9 @@ defmodule Sinema.UserController do
     render conn, "index.html", users: users
   end
 
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Sinema.User, id)
+    render conn, "show.html", user: user
+  end
+
 end
--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,9 +1,10 @@
 defmodule Sinema.UserView do
   use Sinema.Web, :view
   alias Sinema.User
+
   def first_name(%User{name: name}) do
     name
     |> String.split("")
-    |> Enum.at(0)
+    # |> Enum.at(0)
   end
 end
